3d96bf9e4b2d492d93a53b79b3674b5ab3e5f2ae
test <eidan@sleeprate.com>
2014-10-21 10:48:49 +0300

    initial

--- src/main/java/devtest/FizzBuzz.java
+++ src/main/java/devtest/FizzBuzz.java
@@ -7,7 +7,29 @@ public class FizzBuzz {
         this.number = number;
     }
 
+    // TODO - Complete this method
     public String toString() {
-        return null;
+
+        if (number < 0 || number == 0){ //not positive
+            return "Undefined";
+        }
+
+        if (number % 5 == 0 && number % 3 == 0){
+            return "FizzBuzz";
+        }
+
+
+        if (number % 3 == 0){
+            return "Fizz";
+        }
+
+        if (number % 5 == 0){
+            return "Buzz";
+        }
+
+
+
+        return (String.valueOf(number) );
+
     }
 }
--- src/main/java/devtest/Palindrome.java
+++ src/main/java/devtest/Palindrome.java
@@ -1,8 +1,51 @@
 package devtest;
 
+import java.util.ArrayList;
+
 public class Palindrome {
 
-    public static boolean isPalindrome(String str) {
+    // TODO - Complete this method
+    public static boolean isPalindrome(String str)
+    {
+
+        /*ArrayList check = new ArrayList();
+        check.add('a');
+        check.add('b');
+        System.out.println("check size is = " +check.size());*/
+
+       String lowerStr = str.toLowerCase();
+       ArrayList newStr = new ArrayList();
+       newStr = NoSpaceStr (lowerStr);
+
+      // System.out.println(newStr);
+
+       int begin = 0;
+       int end = newStr.size()-1;
+
+       while (begin < end){
+           if (newStr.get(begin) != newStr.get(end)){
+               return false;
+           }
+           if (begin+1 == end && newStr.get(begin) != newStr.get(end) ){
+               return true;
+           }
+           begin++;
+           end--;
+       }
+
         return true;
     }
+
+    protected  static ArrayList NoSpaceStr(String str){
+
+        ArrayList NoSpaceStr = new ArrayList();
+        for (int i=0; i<str.length(); i++){
+            if (str.charAt(i) != ' ' ){
+                NoSpaceStr.add(str.charAt(i));
+            }
+        }
+        return NoSpaceStr;
+    }
+
+
 }
