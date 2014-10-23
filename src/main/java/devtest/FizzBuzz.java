0a0ca9bc2ecd860c1f060f9118a16bb69419fb23
test <eidan@sleeprate.com>
2014-10-23 14:06:23 +0300

    initial

--- src/main/java/devtest/FizzBuzz.java
+++ src/main/java/devtest/FizzBuzz.java
@@ -6,8 +6,26 @@ public class FizzBuzz {
     public FizzBuzz(int number) {
         this.number = number;
     }
-
+    private static final String UNPOSITIVE_INPUT = "Undefined";
+    private static final String DIVIDE_ONLY_BY_3 = "Fizz";
+    private static final String DIVIDE_ONLY_BY_5 = "Buzz";
+    private static final String DIVIDE_BY_3_AND_5 = "FizzBuzz";
     public String toString() {
-        return null;
+        if(number<=0){
+            return UNPOSITIVE_INPUT;
+        }
+        else if(number%15==0){
+            return DIVIDE_BY_3_AND_5;
+        }
+        else if(number%3==0){
+            return DIVIDE_ONLY_BY_3;
+        }
+        else if (number%5==0){
+            return DIVIDE_ONLY_BY_5;
+        }
+        else{
+            return String.format("%d",number);
+        }
+
     }
 }
--- src/main/java/devtest/Palindrome.java
+++ src/main/java/devtest/Palindrome.java
@@ -2,7 +2,28 @@ package devtest;
 
 public class Palindrome {
 
-    public static boolean isPalindrome(String str) {
+    public static boolean isPalindrome(String strInput) {
+        String str = dropSpaces(strInput);
+        int lastIndex = (str.length())/2;
+        int stringSize = str.length()-1;
+        str = str.toLowerCase();
+        for(int i =0 ; i<lastIndex ; ++i){
+            if(str.charAt(i)!=str.charAt(stringSize-i)){
+            return false;
+            }
+        }
         return true;
+    }
+
+    private static String dropSpaces(String s){
+        String retVal = "";
+        char[] inputString = s.toCharArray();
+        int lastIndex = s.length();
+        for(int i =0;i<lastIndex  ;++i){
+            if(inputString[i]!=' '){
+                retVal+=inputString[i];
+            }
+        }
+        return retVal;
     }
 }
