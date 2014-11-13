8ef2bc69526a64b4de925ecba3078b641f99d4aa
test <eidan@sleeprate.com>
2014-11-13 20:12:04 +0200

    initial

--- src/main/java/devtest/FizzBuzz.java
+++ src/main/java/devtest/FizzBuzz.java
@@ -8,6 +8,14 @@ public class FizzBuzz {
     }
 
     public String toString() {
-        return null;
+        if (number<=0)
+            return "Undefined";
+        if (number % 15 ==0)
+            return "FizzBuzz";
+        if (number % 3 == 0)
+            return "Fizz";
+        if (number % 5 == 0)
+            return "Buzz";
+        return  new Integer(number).toString();
     }
 }
--- src/main/java/devtest/Palindrome.java
+++ src/main/java/devtest/Palindrome.java
@@ -3,6 +3,21 @@ package devtest;
 public class Palindrome {
 
     public static boolean isPalindrome(String str) {
-        return true;
+        str = str.toLowerCase();
+        str = String.join("",str.split(" "));
+        int len = str.length();
+        String leftHalf;
+        String rightHalf;
+        if (len % 2 == 0) {
+            leftHalf = str.substring(0,len/2);
+            rightHalf = str.substring(len/2);
+        }
+        else {
+            leftHalf = str.substring(0,len/2);
+            rightHalf = str.substring(len/2+1);
+        }
+        rightHalf = new StringBuilder(rightHalf).reverse().toString();
+        return rightHalf.equals(leftHalf);
     }
+
 }
