7f1447a79a61ac9b301a9adb3d838151ce8e33ac
test <eidan@sleeprate.com>
2014-11-18 20:06:06 +0200

    initial

--- src/main/java/devtest/FizzBuzz.java
+++ src/main/java/devtest/FizzBuzz.java
@@ -8,6 +8,19 @@ public class FizzBuzz {
     }
 
     public String toString() {
-        return null;
+        String ret=null;
+        if (number <1)
+            ret = "Undefined";
+        else if ((number % 3==0) && (number % 5==0))
+            ret = "FizzBuzz";
+        else if ((number % 3==0))
+            ret = "Fizz";
+        else if ((number % 5==0))
+            ret = "Buzz";
+        else
+            ret = Integer.toString(number);
+
+        return ret;
+
     }
 }
--- src/main/java/devtest/Palindrome.java
+++ src/main/java/devtest/Palindrome.java
@@ -1,8 +1,40 @@
 package devtest;
 
+import java.time.Clock;
+
 public class Palindrome {
 
     public static boolean isPalindrome(String str) {
-        return true;
+        String pali = str.replaceAll(" ","");
+
+        pali = pali.toLowerCase();
+
+        return isPalinIter(pali);
+    }
+    public static boolean isPalinIter(String str){
+        int i, j=str.length()-1;
+        Boolean ret = null;
+        for (i=0;i<str.length();i++){
+            if (i==j || i>j) {
+                ret= true;
+                break;
+            }
+            else if (str.charAt(i)!=str.charAt(j)) {
+                ret = false;
+                break;
+
+            }
+            else
+                j--;
+        }
+        return ret;
+    }
+    public static boolean isPalin(String str){
+        if (str.length()==0 || str.length()==1)
+            return true;
+        if (str.charAt(0)!= str.charAt(str.length()-1))
+            return false;
+        else
+            return isPalin(str.substring(1,str.length()-1));
     }
 }
