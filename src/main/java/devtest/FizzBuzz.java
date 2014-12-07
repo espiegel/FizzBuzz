a05a2a77d035fa6efd681599cb95447105717d66
test <eidan@sleeprate.com>
2014-12-07 18:11:56 +0200

    initial

--- src/main/java/devtest/FizzBuzz.java
+++ src/main/java/devtest/FizzBuzz.java
@@ -8,6 +8,25 @@ public class FizzBuzz {
     }
 
     public String toString() {
-        return null;
+
+        if (number <=0){
+            return "Undefined";
+        }
+
+        else if (number%5 == 0 && number%3 ==0){
+            return "FizzBuzz";
+        }
+
+        else if (number%5==0){
+            return "Buzz";
+        }
+
+        else if (number%3==0){
+            return "Fizz";
+        }
+
+
+
+        return Integer.toString(number);
     }
 }
--- src/main/java/devtest/Palindrome.java
+++ src/main/java/devtest/Palindrome.java
@@ -3,6 +3,30 @@ package devtest;
 public class Palindrome {
 
     public static boolean isPalindrome(String str) {
+        String strLowerCase = str.toLowerCase();
+        String[] strCut = strLowerCase.split(" ");
+        int counter = 0;
+        String newStr="";
+
+        for (int i=0; i<strCut.length;i++){
+            newStr += strCut[i];
+        }
+
+
+        //reverse String
+        String reverseStr ="";
+        for (int j=newStr.length()-1; j>=0;j--){
+            reverseStr += newStr.charAt(j);
+        }
+
+        for (int k=0; k<newStr.length();k++){
+            if (newStr.charAt(k)!=reverseStr.charAt(k)){
+                return false;
+            }
+
+        }
+
+
         return true;
     }
 }
