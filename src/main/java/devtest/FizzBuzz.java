e0624130884876b33ace7e482b2bd86acba71fb1
test <eidan@sleeprate.com>
2014-12-15 18:41:59 +0200

    initial

--- src/main/java/devtest/FizzBuzz.java
+++ src/main/java/devtest/FizzBuzz.java
@@ -8,6 +8,10 @@ public class FizzBuzz {
     }
 
     public String toString() {
-        return null;
+        if(number <= 0) return "Undefined";
+        else if((number % 15) == 0) return "FizzBuzz";
+        else if((number % 5) == 0) return "Buzz";
+        else if((number % 3) == 0) return "Fizz";
+        else return Integer.toString(number);
     }
 }
--- src/main/java/devtest/Palindrome.java
+++ src/main/java/devtest/Palindrome.java
@@ -1,8 +1,14 @@
 package devtest;
 
+import com.sun.org.apache.xml.internal.security.algorithms.JCEMapper;
+
 public class Palindrome {
 
     public static boolean isPalindrome(String str) {
-        return true;
+        String ret_str = str.toLowerCase().replaceAll(" ","");
+        boolean flag = true;
+        int i = 0; int j = ret_str.length() - 1 ;
+        for(;i<=j; i++,j--) if (ret_str.charAt(i) != ret_str.charAt(j)) flag = false;
+        return flag;
     }
 }
